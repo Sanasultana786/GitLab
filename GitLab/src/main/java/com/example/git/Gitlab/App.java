aa35877e6275e4c3d3a95aef3cb6ea440395305a
Sana Sultana <Admin@DESKTOP-U3LV6LV>
2022-09-12 07:53:40 -0700

    First Commit

---
+++ GitLab/src/main/java/com/example/git/Gitlab/App.java
@@ -0,0 +1,7 @@
+package com.example.git.Gitlab;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ GitLab/src/test/java/com/example/git/Gitlab/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.git.Gitlab;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
